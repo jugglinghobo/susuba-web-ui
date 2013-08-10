cefd1f8b0286db102cc4e177ddf0ce58733eac50
Simon Kiener <jugglinghobo@gmail.com>
2013-08-10 15:53:25 +0200

    changed adapter to restadapter

--- source/javascripts/models/store.js
+++ source/javascripts/models/store.js
@@ -1,8 +1,12 @@
 App.Store = DS.Store.extend({
   revision  : 12,
-  adapter   : 'App.LSAdapter'
+  adapter   : 'App.SusubaAdapter'
 });
 
 App.LSAdapter = DS.LSAdapter.extend({
   namespace : 'beers-emberjs'
+});
+
+App.SusubaAdapter = DS.RESTAdapter.extend({
+  url : "http://susuba.herokuapp.com"
 });
