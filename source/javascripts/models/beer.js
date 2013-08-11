c62151d27a4d1ca5c690f1a1aabdec01335309ed
Simon Kiener <jugglinghobo@gmail.com>
2013-08-11 16:22:47 +0200

    working on serializing json correctly

--- source/javascripts/models/beer.js
+++ source/javascripts/models/beer.js
@@ -1,5 +1,4 @@
 App.Beer = DS.Model.extend({
   name    : DS.attr('string'),
-  rating  : DS.attr('string'),
-  tasted  : DS.attr('boolean')
+  alc     : DS.attr('string')
 });
--- source/javascripts/models/store.js
+++ source/javascripts/models/store.js
@@ -8,5 +8,32 @@ App.LSAdapter = DS.LSAdapter.extend({
 });
 
 App.SusubaAdapter = DS.RESTAdapter.extend({
-  url : "http://susuba.herokuapp.com"
+  url : "http://localhost:3000",
+
+  transform: function(json) {
+    beers = []
+    json.forEach(function(beer) {
+      obj = JSON.parse(beer);
+      obj.id = obj._id;
+      delete obj._id;
+      beers.push(obj);
+    });
+    json = JSON.stringify({beers: beers});
+    console.log(json);
+    return json;
+  },
+  
+  findAll: function(store, type, since) {
+    var root, adapter;
+
+    root = this.rootForType(type);
+    adapter = this;
+
+    return this.ajax(this.buildURL(root), "GET",{
+      data: this.sinceQuery(since)
+    }).then(function(json) {
+      json = adapter.transform(json);
+      adapter.didFindAll(store, type, json);
+    }).then(null, DS.rejectionHandler);
+  }
 });
