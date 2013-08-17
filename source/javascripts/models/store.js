273f0ed5bdf2f6a2550615609474101dff8f5401
Simon Kiener <jugglinghobo@gmail.com>
2013-08-17 03:02:17 +0200

    moved from JSONSerializer to RESTSerializer

--- source/javascripts/models/store.js
+++ source/javascripts/models/store.js
@@ -8,35 +8,14 @@ App.LSAdapter = DS.LSAdapter.extend({
 });
 
 App.SusubaAdapter = DS.RESTAdapter.extend({
+  bulkCommit: false,
   url : "http://localhost:3000",
-  serializer: DS.JSONSerializer.extend({
 
-    extractMany: function(loader, json, type, records) {
-      var root = this.rootForType(type);
-      root = this.pluralize(root);
-      json = this.transform_IdToId(root, json);
-      if (json[root]) {
-        var objects = json[root], references = [];
-        if (records) { records = records.toArray(); }
+  serializer: DS.RESTSerializer.extend({
 
-        for (var i = 0; i < objects.length; i++) {
-          if (records) { loader.updateId(records[i], objects[i]); }
-          var reference = this.extractRecordRepresentation(loader, type, objects[i]);
-          references.push(reference);
-        }
-
-        loader.populateArray(references);
-      }
-    },
-
-    transform_IdToId: function(root, json) {
-      json[root].forEach(function(object) {
-        object.id = object._id;
-        delete object._id;
-      });
-      return json;
+    primaryKey: function(type) {
+      return '_id';
     }
-
   }),
 });
 
