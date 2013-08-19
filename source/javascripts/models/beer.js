206e0b81c5aa541f52cee52ec17666920cf7eea3
Simon Kiener <jugglinghobo@gmail.com>
2013-08-19 19:54:23 +0200

    made alc a number instead of string

--- source/javascripts/models/beer.js
+++ source/javascripts/models/beer.js
@@ -1,6 +1,6 @@
 App.Beer = DS.Model.extend({
   name    : DS.attr('string'),
-  alc     : DS.attr('string'),
+  alc     : DS.attr('number'),
   rating  : DS.attr('string'),
   tasted  : DS.attr('boolean'),
 });
